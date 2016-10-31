b6a86fd550837978ce1e76fed85b77550d028b13
jonnyalexbh <jonnyalex.bh@gmail.com>
2016-10-31 09:50:24 -0500

    Middleware entrust

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,7 @@ class UsersController extends Controller {
 
   public function __construct(){
     $this->middleware('auth', ['only' => ['index', 'edit', 'create']]);
+    $this->middleware('role:admin');
   }
   /**
   * Display a listing of the resource.
